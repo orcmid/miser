fa829de88e60169ac8414acf0b1ec713b354b361
Dennis E. Hamilton <dennis.hamilton@acm.org>
2026-01-11 09:18:00 -0800

    0.0.1 oFrugal.c Identified as REPL Sprout

---
+++ devLab/oFrugal/dev/src/oFrugal.c
@@ -0,0 +1,45 @@
+/* oFrugal.c 0.0.1                  UTF-8                         2026-01-11
+ * -|----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
+ *
+ *                      THE MISER PROJECT ON GITHUB
+ *
+ *                           OFRUGAL REPL PROGRAM
+ *                           ====================
+ *
+ *   This is crutch coding to sort of bootstrap our way into the project
+ *   and have something that is always running as this all gets figured out.
+ *
+ *            who the heck is by Dr. Dean Earl Wright III (from copilot)
+ */
+
+
+#include <stdio.h>      /* fputs */
+
+#include <stdlib.h>     /* exit */
+
+
+
+int main(void)
+{
+
+  fputs("\n[of] oFrugal 0.0.1 Miser REPL Sprout\n", stdout);
+
+  fputs("\n     You find yourself in a dark room with no windows", stdout);
+  fputs("\n     and only one door. There is a table in the middle", stdout);
+  fputs("\n     of the room. On the table is a small box.", stdout);
+  fputs("\n     What do you do?\n", stdout);
+  fputs("\n     ... OH WAIT, strike that set!!\n", stdout);
+
+  fputs("\n     Nothing to see here, move along\n", stdout);
+
+  fputs("\n   > ^C", stdout);
+
+  exit(EXIT_SUCCESS);
+
+ }
+/*
+   0.0.1 2026-01-11T17:11Z Identify as REPL sprout.
+   0.0.0 2026-01-09T22:16Z Tiny nothing-to-see stub to kick-start the code.
+   */
+
+/*                        ***** end of oFrugal.c *****                      */
