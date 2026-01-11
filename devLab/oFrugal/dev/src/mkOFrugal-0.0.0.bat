4bb8ba8f5f64d8b6cd5e082a838de29e76e11fa3
Dennis E. Hamilton <dennis.hamilton@acm.org>
2026-01-11 09:44:37 -0800

    src/mkOFrugal-0.0.0 temporary materials for compiling oFrugal sprouts
    Doing this in-place until there is some oMiser to include in more-sophisticated builds

---
+++ devLab/oFrugal/dev/src/mkOFrugal-0.0.0.bat
@@ -0,0 +1,15 @@
+@echo off
+REM mkOFrugal-0.0.0.bat 0.0.0       UTF-8                         2026-01-09
+rem |----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
+rem
+rem                   COMPILING oFrugal-0.0.0
+rem
+rem    Compile oFrugal.exe crutch code with optionization for maximum speed.
+
+CL @VCoptions.opt oFrugal.c
+
+rem |----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
+rem
+rem 0.0.0  2026-01-09T21:55Z Initial version for simple crutching in
+rem
+rem                  *** end of mkOFrugal-0.0.0.bat ***
