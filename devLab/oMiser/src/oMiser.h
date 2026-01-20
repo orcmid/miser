eeb706c48f17a1c6c4e4a686261f72ce061dbf43
Dennis E. Hamilton <dennis.hamilton@acm.org>
2026-01-20 12:08:32 -0800

    0 .0.0 evLab/oMiser/oMiser.h skeleton placeholder
    Preparing to have it work in both C and C++.

---
+++ devLab/oMiser/src/oMiser.h
@@ -0,0 +1,29 @@
+/* oMiser.h 0.0.0                   UTF-8                         2026-01-20
+ * -|----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
+ *
+ *                             The oMiser API
+ *                             ==============
+ *
+ *   This is the spirally-developed oMiser API.  The API is designed for use
+ *   on any x64 platform, but the C++ support may depend on Microsoft Visual
+ *   Studio and Windows SDKs for some of its features.
+ *
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+/*
+ * -|----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
+ *
+ *  0.0.0 2026-01-20T19:38Z Empty skeleton
+ *
+ *                        *** end of oMiser.h ***
+ */
