2b969b2fbc959d323d750ff2100b9e8a535de23e
Dennis E. Hamilton <dennis.hamilton@acm.org>
2026-01-29 16:05:14 -0800

    0.0.1 Introduce omEstablish and omOrigami CLSID

--- devLab/oMiser/src/oMiser.hpp
+++ devLab/oMiser/src/oMiser.hpp
@@ -1,4 +1,4 @@
-/* oMiser.hpp 0.0.0                 UTF-8                         2026-01-29
+/* oMiser.hpp 0.0.1                 UTF-8                         2026-01-29
  * -|----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
  *
  *                           The oMiser C++ API
@@ -11,9 +11,26 @@
  *
  */
 #ifndef __cplusplus
-#error oMiser.hpp provides the C++ API.  Use oMIser.h for the C Language API.
+#error oMiser.hpp provides the C++ API only.  Use oMiser.h for C Language.
 #endif
+
+#include <WinNT.h>       // For HRESULT definition
+#include <guiddef.h>     // For GUID, IID, CLSID definitions
+
 extern "C" {
+
+HRESULT omEstablish(CLSID* omClassID, IID* riid, void **ppvObject);
+
+
+    /* The Class ID for the Origami version of the oMiser engine.
+       {476EAF02-4524-4D81-9227-6272464959CB}
+       */
+static const CLSID omOrigami
+             = { 0x476eaf02, 0x4524, 0x4d81,
+                   { 0x92, 0x27, 0x62, 0x72, 0x46, 0x49, 0x59, 0xcb }
+                 };
+
+
 
 
 
@@ -24,6 +41,7 @@ extern "C" {
 /*
  * -|----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
  *
+ *  0.0.1 2026-01-29T23:57Z Introduce omEstablish and omOrigami CLSID
  *  0.0.0 2026-01-29T19:18Z Empty skeleton
  *
  *                         *** end of oMiser.hpp ***
