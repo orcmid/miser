388941592515d8a7f9c343a050a419fca478b7f4
Dennis E. Hamilton <dennis.hamilton@acm.org>
2026-02-03 10:09:48 -0800

    0.0.5 oMiser/src/oMIser-Win32.h Introduce IUnknown structures

--- devLab/oMiser/src/oMiser-Win32.h
+++ devLab/oMiser/src/oMiser-Win32.h
@@ -1,4 +1,4 @@
-/* oMiser-Win32.h 0.0.4             UTF-8                         2026-02-01
+/* oMiser-Win32.h 0.0.5             UTF-8                         2026-02-02
  * -|----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
  *
  *                             The oMiser API
@@ -6,7 +6,7 @@
  *
  *   This is the spirally-developed oMiser API.  The API is designed for use
  *   on any x64 platform, but this code depends on Microsoft Visual Studio
- *   Code and Microsoft Windows SDK libraries.
+ *   Code, Build Tools, and Microsoft Windows SDK libraries.
  *
  */
 
@@ -28,20 +28,42 @@ HRESULT omEstablish(CLSID* omClassID, IID* riid, void **ppvObject);
        FIXIT: Identify the possible HRESULT return values.
        */
 
-static const CLSID omSpitball
+static const CLSID CLSID_omSpitball
              = { 0x476eaf02, 0x4524, 0x4d81,
                    { 0x92, 0x27, 0x62, 0x72, 0x46, 0x49, 0x59, 0xcb }
                  };
 
     /* The Class ID for the Spitball version of the oMiser engine:
        {476EAF02-4524-4D81-9227-6272464959CB}.  This is very much pre-flight
-       and having no treasured air/space flight significance.  Ths is the
+       and having no treasured air-/space-flight significance.  Ths is the
        CLSID for initial stumblings toward a meaningful omEstablish.
 
        The use of CLSIDs in this manner allows determination whether there is
        agreement between the header used and the oMiser engine code used
        at runtime.
        */
+
+static const IID IID_IUnknown
+                 /* The Interface that every component supports and that
+                    every interface begins with (derives from) */
+             = { 0x00000000, 0x0000, 0x0000,
+                   { 0xC0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x46 }
+                 };
+
+#define IUNKNOWN_V \
+        HRESULT (*QueryInterface)(void* This, IID* riid, void **ppv); \
+        ULONG (*AddRef)(void* This); \
+        ULONG (*Release)(void* This)
+        /* The standard IUnknown interface methods supplied at the beginning
+           of every COM interface vtable
+           */
+
+typedef struct IUnknownV { IUNKNOWN_V; } IUnknownV;
+    /* Just enough vtable for an instance's IUnknown interface */
+
+typedef struct IUnknown { IUnknownV *pv; } IUnknown;
+    /* An instance's IUnknown interface */
+
 
 #ifdef __cplusplus
 }
@@ -50,6 +72,7 @@ static const CLSID omSpitball
 /*
  * -|----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
  *
+ *  0.0.5  2026-02-02T18:04Z Introduce IUnknownV and IUnknown structures
  *  0.0.4  2026-02-01T17:39Z Remove Unknown reference, rename as Win32 version
  *  0.0.3  2026-01-31T20:33Z Initial staging for oMiser spitball
  *  0.0.2  2026-01-30T18:06Z Align with 0.0.1 oMiser.hpp
