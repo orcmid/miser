d316e2ecc0a3709e0be4045107a8cf16dd2a7750
Dennis E. Hamilton <dennis.hamilton@acm.org>
2026-02-07 09:50:13 -0800

    0.0.9 oMiser-Win32.h smoothing and OM_E_CLSID_UNSUPPORTED definition

--- devLab/oMiser/src/oMiser-Win32.h
+++ devLab/oMiser/src/oMiser-Win32.h
@@ -1,4 +1,4 @@
-/* oMiser-Win32.h 0.0.8             UTF-8                         2026-02-05
+/* oMiser-Win32.h 0.0.9             UTF-8                         2026-02-07
  * -|----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
  *
  *                             The oMiser API
@@ -6,31 +6,33 @@
  *
  *   This is the spirally-developed oMiser API.  The API is designed for use
  *   on any x64 platform, but this code depends on Microsoft Visual Studio
- *   Code, Build Tools, and Windows SDK libraries.
+ *   Code, Build Tools, and SDK libraries.
  *
  *   The Component Object Model (COM) is used to provide application program
  *   interfaces (APIs) that separate implementation concerns and provide for
  *   modularity.  COM also allows for networked operation without the need for
  *   applications to attend to network details or even the presence of
  *   networked operation.  This enables distributed computing scenarios,
- *   although that is not an immediate objective.
+ *   although that is not an immediate Miser Project objective.
  *
  *   COM-based reusable software components can be easily integrated into
  *   different applications and also updated without affecting the clients
  *   that use them, so long as the interfaces remain consistent.  Such safe
- *   versioning of interfaces is a key provision of COM.
+ *   versioning of interfaces is a key provision of COM that is valuable for
+ *   oMiser..
  *
  * -|----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
  *
  *   This header provides the Win32-specific definitions for the oMiser API.
  *   Although most documentation and application of COM is oriented to the
- *   use of C++, this header provides the necessary definitions for C and
- *   the implementation of oMiser is also in C Language.
+ *   use of C++, this header provides the necessary definitions for C since
+ *   the implementations of oMiser and oFrugal are in C Language.  There is
+ *   C++ specific support in oMiser-Win32.hpp.
  */
 
 #include <WinNT.h>       // For HRESULT definition
 #include <guiddef.h>     // For GUID, IID, CLSID definitions
-#include <WinDef.h>      // For basic Windows types
+#include <WinDef.h>      // For basic Windows types used in COM interfaces
 #include <Winerror.h>    // For named HRESULT error codes
 
 #ifdef __cplusplus
@@ -43,12 +45,22 @@ HRESULT omEstablish(CLSID* omClassID, IID* riid, void **ppvObject);
     /* The "factory" interface for establishing an oMiser universe instance.
        There is never more than one instance of the oMiser engine.  If the
        engine is already established, operation is simply a QueryInterface
-       on that existing instance.
+       on that existing component.
 
-       FIXIT: Identify the possible HRESULT return values.  We'll also need
-              to define successive Class IDs and additional Interface
-              definitions as the API evolves.
+       HRESULT values returnable by omEstablish include:
+
+                         S_OK: The operation succeeded.
+                    E_POINTER: One or more arguments are NULL.
+                E_NOINTERFACE: The requested interface is not supported.
+                    E_NOTIMPL: The requested interface is not implemented.
+                       E_FAIL: The operation failed.
+                 E_UNEXPECTED: An unexpected error occurred.
+                E_OUTOFMEMORY: Insufficient memory to complete the operation.
+       OM_E_CLSID_UNSUPPORTED: The specified CLSID is not supported.
        */
+
+#define OM_E_CLSID_UNSUPPORTED \
+        (HRESULT)(MAKE_HRESULT(0x1, FACILITY_ITF, 0x200))
 
 static const CLSID CLSID_omSpitball
              = { 0x476eaf02, 0x4524, 0x4d81,
@@ -61,7 +73,8 @@ static const CLSID CLSID_omSpitball
 
        The use of a CLSIDs progression allows confirmation of compatibility
        between the use of this header and the oMiser engine runtime code
-       actually linked to the application.
+       actually linked to the application.  It is conceivable that multiple
+       CLSIDs might be preserved as part of a versioning strategy.
        */
 
 /* IUNKNOWN INTERFACE CONSTRUCTIONS */
@@ -94,16 +107,16 @@ typedef struct IUnknownV { IUNKNOWN_V; } IUnknownV;
        the V suffix is for vtable types, all of which will begin with
        IUNKNOWN_V.  Given any interface, additional interfaces are
        obtained through the interface's Query Interface method.  Here,
-       just the IUnknown methods are presumed.
+       just the (likely initial)IUnknown methods are presumed.
        */
 
 typedef struct IUnknownPV { IUnknownV *pv; } IUnknownPV;
     /* A pointer to a vtable, here presuming just the IUnknown methods
-       provided for every component.
+       that every component must support.
        */
 
 typedef IUnknownPV *pIUnknown;
-    /* A pointer to the vtable pointer in an instance's state. All that
+    /* A pointer to the vtable pointer in a component's state. All that
        is assumed about that state is its vtable pointer location and the
        methods that are exposed thereby.
        */
@@ -121,7 +134,7 @@ typedef IUnknownPV *pIUnknown;
        UINT refsLeft = pThat->pv->Release( pThat );
 
        All COM component instances have reusable vtables and the methods must
-       operate on the particular instance state the method applies to.
+       operate on the specific component state the method applies to.
        The methods are informed of that with their first parameter.
 
        FIXME: See if there is a way to abbreviate these calls somehow.
@@ -135,6 +148,7 @@ typedef IUnknownPV *pIUnknown;
 /*
  * -|----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
  *
+ *  0.0.9  2026-02-07T17:13Z Refine comments and define OM_E_CLSID_UNSUPPORTED
  *  0.0.8  2026-02-05T23:52Z Improve namings and explanations of the pattern.
  *  0.0.7  2026-02-05T00:55Z Clean up the types and HRESULT definitions
  *  0.0.6  2026-02-04T23:09Z Touch-up comments and formatting
