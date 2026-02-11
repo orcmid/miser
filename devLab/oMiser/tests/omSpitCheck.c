f5c684e6da673aab5b56fc0e2e3ba2f948c81fb0
Dennis E. Hamilton <dennis.hamilton@acm.org>
2026-02-10 19:49:56 -0800

    0.0.4 oMiser/Tests/omSpitCheck.c Complete draft

--- devLab/oMiser/tests/omSpitCheck.c
+++ devLab/oMiser/tests/omSpitCheck.c
@@ -1,4 +1,4 @@
-/* omSpitCHeck.c 0.0.2              UTF-8                         2026-02-10
+/* omSpitCHeck.c 0.0.4              UTF-8                         2026-02-11
  * -|----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
  *
  *                      THE MISER PROJECT ON GITHUB
@@ -25,7 +25,7 @@ static const CLSID CLSID_omSpitballCheck
              = { 0x476eaf02, 0x4524, 0x4d81,
                    { 0x92, 0x27, 0x62, 0x72, 0x46, 0x49, 0x59, 0xcb }
                  };
-       /* The class we expect to be supported.  We do this, in particular,
+       /* The class we expect to be supported.  We declare this here
           because we don't know what might be set with oMiser-Win32.h
           */
 
@@ -36,96 +36,205 @@ extern const IUnknownV IomSpitballV;
 typedef struct {const IUnknownV *pv; volatile long refs;} omSpitball;
     /* A minimal COM state for Spitball testing.  It just has
        reference counts.  This is the expected beginning of the
-       static omGlobalState implemented starting with Spitball
-       versions of omiser-Win32.c.
+       static omGlobalState commencing with Spitball versions of
+       omiser-Win32.c.
        */
 
 extern omSpitball omGlobalState;
-       /* The ordinarily-private fixed omSpitball state  */
+       /* The ordinarily-private omSpitball state location */
 
-    /* FIXME: This won't work if the names are different in the linked
+    /* FIXME: This won't work if the name is different in the linked
               oMiser-Win32.c.  It's not clear the linker will catch
-              that.
-                 We need an agreement on these two externs that is
-              not dependent on the supported CLSID(s) of the linked
-              oMiser-Win32.c and applicable oMiser-Win32.h.  We then
-              need to figure out which CLSIDs are supported by the linked
-              oMiser-Win32.c and applicable oMiser-Win32.h and which cannot
-              be supported any longer because of incompatibilities.
+              that.  That's why omGlobalState is not tied to the CLSID
+              or code version, to ensure linker agreement always.
+                 We might support multiple CLSIDs in the future, and it is
+              desirable that earlier ones remain workable as further ones
+              essentially inherit the more-limited interfaces and global
+              state portions of earlier ones.
+                 This might be a pipe-dream, but it would be nice having
+              tests be reusable until they aren't.
               */
 
 int main(void)
 {
 
-  fputs("\n[om] omSpitCheck 0.0.2 Confirmation of oMiser Spitball", stdout);
+  fputs("\n[om] omSpitCheck 0.0.4 Confirmation of oMiser Spitball", stdout);
 
   /* CONFIRM ACCESS AND STATUS OF UNESTABLISHED omGlobalState */
+  /* -------------------------------------------------------- */
+
   fputs("\n"
-        "\n     Checking unestablished omGlobalState ...", stdout);
+        "\n     Checking for unestablished omGlobalState ...", stdout);
 
   if (omGlobalState.pv != NULL)
       { fputs("\n     ERROR: omGlobalState.v is not NULL\n\n", stdout);
         exit(EXIT_FAILURE);
         }
-
   if (omGlobalState.refs != 0)
       { fputs("\n     ERROR: omGlobalState.refs is not 0\n\n", stdout);
         exit(EXIT_FAILURE);
         }
-
-  fputs("\n     omGlobalState seems correctly not yet established", stdout);
+  fputs("\n     success: omGlobalState present, not established", stdout);
 
   /* CONFIRM DEFENSES OF omEstablish API */
-  const CLSID CLSID_omFowlTip         /* A never-supported oMiser CLSID */
+  /* ----------------------------------- */
+
+  const CLSID CLSID_omFoulTip         /* A never-supported oMiser CLSID */
               = { 0xb51e2358, 0xadad, 0x4509,
                     { 0xb0, 0xd1, 0xf0, 0x2a, 0xd, 0x14, 0x78, 0x37 }
                   };
 
-  /* FIXME: CREATE NON-NULL IUnknown REFIID Pointer to confirm that
-            it is not altered by any of the pointer errors
-            */
+  const CLSID CLSID_omSpitball        /* Required for oMiser Spitball */
+             = { 0x476eaf02, 0x4524, 0x4d81,
+                   { 0x92, 0x27, 0x62, 0x72, 0x46, 0x49, 0x59, 0xcb }
+                 };
+
+  const IID IID_Fake                  /* A never-supported IID */
+              ={ 0x19c50c7b, 0x52ac, 0x4cf3,
+                   { 0x80, 0x62, 0x5f, 0x26, 0xc7, 0xa, 0x66, 0x16 }
+                 };
+
+  const IID IID_IUnknown
+              = { 0x00000000, 0x0000, 0x0000,
+                    { 0xC0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x46 }
+                  };
+
+  IUnknown  *pIUnknown = (void *)0xDEADBEEF01234567;
+            /* A non-NULL pointer to confirm no alteration on any of the
+               pointer errors.
+               */
 
   fputs("\n"
         "\n     Checking omEstablish Defenses ...", stdout);
 
-  /* FIXME: March NULL parameters through calls on omEstablish,
+        /*  March NULL parameters through calls on omEstablish,
             Each should get E_POINTER error and the REFIID pointer
             should not be touched.
             */
 
-  /* FIXME: Have a valid omEstablish call but with CLSID_omFowlTip
-            causing OM_E_CLSID_UNSUPPORTED *and* the REFIID has
-            been NULLed.
-            */
+  HRESULT hr = omEstablish( NULL, &IID_IUnknown, (void **)&pIUnknown);
+  if (hr != E_POINTER)
+      { fputs("\n     ERROR: no E_POINTER for NULL CLSID\n\n", stdout);
+        exit(EXIT_FAILURE);
+        }
 
-  /* FIXME: Have a valid CLSID_omSpitball call to omEstablish but
-            not a recognized IID parameter.  This will again NULL
-            the REFIID parameter.  The global state will be established,
-            but no interface will be delivered.  The refs count should
-            be 0, since no reference has been delivered.  CONFIRIM
-            THIS CASE.
-            */
+  hr = omEstablish( &CLSID_omFoulTip, NULL, (void **)&pIUnknown);
+  if (hr != E_POINTER)
+      { fputs("\n     ERROR: no E_POINTER for NULL IID*\n\n", stdout);
+        exit(EXIT_FAILURE);
+        }
 
-  /* FIXME: Have a valid call on omEstablish and now confirm that
-            Global State is delivered *and*, the expected VTable is
-            there, and the refs value is 1.
-            */
+  hr = omEstablish( &CLSID_omFoulTip, &IID_IUnknown, NULL);
+  if (hr != E_POINTER)
+      { fputs("\n     ERROR: no E_POINTER for NULL IUnknown ppv\n\n", stdout);
+        exit(EXIT_FAILURE);
+        }
 
-  /* FIXME: Confirm that the delivered interface is functional by calling
-            AddRef and Release and confirming the expected changes in
-            refs values returned.   Because it is going to dive into the
-            methods, this and an unsuccessful and successful QueryInterface
-            should also be checked.
-            */
+  if (pIUnknown != (void *)0xDEADBEEF01234567)
+      { fputs("\n     ERROR: pIUnknown pointer was altered prematurely\n\n",
+              stdout
+              );
+        exit(EXIT_FAILURE);
+        }
+
+  fputs("\n     checked: E_POINTER resulted for each NULL parameter case.",
+        stdout);
+
+
+  /* CONFIRM CLSID VERIFICATION */
+  /* -------------------------- */
 
   fputs("\n"
-        "\n     MORE CHECKING TBD. STAY TUNED\n\n", stdout);
+        "\n     Checking CLSID Verification ...", stdout);
+
+  hr = omEstablish( &CLSID_omFoulTip, &IID_Fake, (void **)&pIUnknown);
+
+  if (pIUnknown != NULL)
+       fputs("\n     warning: no precautionary ppv clearing done.", stdout);
+
+  if (hr != OM_E_CLSID_UNSUPPORTED)
+      { fputs("\n     ERROR: unsupported CLSID not rejected.\n\n", stdout);
+        exit(EXIT_FAILURE);
+        }
+
+  hr = omEstablish( &CLSID_omSpitball, &IID_Fake, (void **)&pIUnknown);
+
+  if (hr != OM_E_CLSID_UNSUPPORTED)
+       fputs("\n     omSpitball CLSID likely accepted.", stdout);
+
+  /* Note: Since we didn't get OM_E_CLSID_UNSUPPORTED now, assume that
+     the likely case is  E_NOINTERFACE because of &IID_Fake.
+     There should not be any other result in the omSpitball case,
+     so might have to punt otherwise.
+     */
+
+  if (hr != E_NOINTERFACE)
+       { fputs("\n     UNEXPECTED HRESULT: something has gone wrong.\n\n",
+               stdout);
+         exit(EXIT_FAILURE);
+            }
+
+  /* CONFIRM GLOBAL STATE ESTABLISHED */
+  /* -------------------------------- */
+  /* We have NO_INTERFACE but we'll get around to that after we deal with
+     the fact that the Spitball CLSID was accepted and there should now
+     be an initialized global state.
+     */
+
+  fputs("\n"
+        "\n     Confirming establishment of Global State ...", stdout);
+
+  if (omGlobalState.pv == NULL)
+       { fputs("\n     ERROR: Global State not established\n\n", stdout);
+         exit(EXIT_FAILURE);
+         }
+
+  if (omGlobalState.pv != &IomSpitballV)
+       fputs("\n     warning: omGlobalState is not for Spitball",
+             stdout);
+  else fputs("\n     omGlobalState is for Spitball", stdout);
+
+
+  /* CONFIRM INTERFACE OBTAINED */
+  /* -------------------------- */
+  /* account for the E_NOINTERFACE and go again to get a recognized
+     interface of the already-established spitball and Global State.
+     */
+
+  fputs("\n"
+        "\n     Checking Interface IID Verification ..."
+        "\n     E_NOINTERFACE received as expected for IID_Fake.", stdout);
+
+
+  hr = omEstablish( &CLSID_omSpitball, &IID_IUnknown, (void **)&pIUnknown);
+
+  if (hr != S_OK)
+       { fputs("\n     ERROR: IUnknown not obtained\n\n", stdout);
+         exit(EXIT_FAILURE);
+         }
+
+  fputs("\n     IUnknown interface received as expected", stdout);
+
+  if (2 != pIUnknown->pv->AddRef(pIUnknown))
+       fputs("\n     IUnknown AddRef did not return expected value 2\n\n",
+               stdout
+               );
+
+  if (1!= pIUnknown->pv->Release(pIUnknown))
+       fputs("\n     IUnknown Release did not return expected value 1\n\n",
+               stdout
+               );
+
+  fputs("\n"
+        "\n     omSpitCheck completed.\n\n", stdout);
 
 
   exit(EXIT_SUCCESS);
 
  }
 /*
+   0.0.4  2026-02-11T03:44Z First complete draft of omSpitCheck.c.
+   0.0.3  2026-02-10T20:04Z Setup before checking E_POINTER cases
    0.0.2  2026-02-10T18:37Z Add FIXMEs for the additional checsk needed
    0.0.1  2026-02-10T02:39Z Checking for unestablished omGlobalState.
    0.0.0  2026-02-07T23:44Z Tiny nothing-to-see stub to kick-start the code.
