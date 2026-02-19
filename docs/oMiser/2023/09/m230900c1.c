34a9d59a767fc2d0582428a2e7ab83e158266de8
Dennis E. Hamilton <dennis.hamilton@acm.org>
2026-02-19 11:25:32 -0800

    0.2.0 oMiser m230900c.md clean-up wordings, examples

--- docs/oMiser/2023/09/m230900c1.c
+++ docs/oMiser/2023/09/m230900c1.c
@@ -1,4 +1,4 @@
-/* m230900c1.c 0.0.0                UTF-8                         2026-02-18
+/* m230900c1.c 0.0.1`                UTF-8                         2026-02-19
  * -|----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
  *
  *                     IMPLEMENTATION OF OB COMPARISON
@@ -11,21 +11,26 @@
 typedef struct ob {ulong refs; ulong hashflag;
                    struct ob *a; struct ob *b;} ob;
         /* just something all ob-cells have in common, ignoring
-           COM niceties */
+           COM niceties which don't matter internal to oMiser here */
 
 bool q(ob *x, ob *y)
   {  if (x == y) return true;  // same address, same ob
      if (x->hashflag != y->hashflag) return false;
      if (x->a == x) return false;  // same individual only if same address
-     if (x->b == x) return q(x->a, y->a); // check enclosure a-parts
+     if (x->b == x) return q(x->a, y->a); // so singleton, and if not
      return q(x->a, y->a) && q(x->b, y->b); // must be two pairs
     }
 
 /* Here, (x->a == x) is C/C++ for is-individual(x) and
  *       (x->b == x) is C/C++ for is-singleton(x)
+ *
+ * In the actual implementation, the Flag byte provides this information.
+ * There is also presumption that comparing pointers for == and != is
+ * reliable in the world of x64 processors.
  */
 
 /*
- *  0.0.0 2026-02-18T21:17Z Trial code - not yet tested.
+ *  0.0.2 2026-02-19T18:54Z Expanded commentary
+ *  0.0.1 2026-02-18T21:17Z Trial code - not yet tested.
  *
  *                *** end of m230900c1.c ***                      */
--- docs/oMiser/2023/09/m230900c2.c
+++ docs/oMiser/2023/09/m230900c2.c
@@ -1,4 +1,4 @@
-/* m230900c2.c 0.0.0                UTF-8                         2026-02-19
+/* m230900c2.c 0.0.1                UTF-8                         2026-02-19
  * -|----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
  *
  *                     IMPLEMENTATION OF OB COMPARISON
@@ -19,29 +19,26 @@ bool q(ob *x, ob *y); // forward declaration
 
 bool qa(ob **px, ob **py)
   { if ( !q(a(*px), a(*py)) ) return false;
-    px->a = py->a = oldest(*px, *py);
+    px->a = py->a = preferable(*px, *py);
     return true;
     }
 
 bool qb(ob **px, ob **py)
   { if ( !q(b(*px), b(*py)) ) return false;
-    px->b = py->b = oldest(*px, *py);
+    px->b = py->b = preferable(*px, *py);
     return true;
     }
 
 bool q(ob *x, ob *y)
-  {  if (x == y) return true;  // same address, same ob
+  {  if (x == y) return true;
      if (x->hashflag != y->hashflag) return false;
-     if (x->a == x) return false;  // same individual only if same address
-     if (x->b == x) return qa(x, y); // check enclosure a-parts
-     return qa(x, y) && qb(x, y); // must be two pairs
+     if (x->a == x) return false;
+     if (x->b == x) return qa(x, y);
+     return qa(x, y) && qb(x, y);
     }
 
-/* Here, (x->a == x) is C/C++ for is-individual(x) and
- *       (x->b == x) is C/C++ for is-singleton(x)
- */
-
 /*
+ *  0.0.1 2026-02-19T19:13Z Clean-up
  *  0.0.0 2026-02-19T00:20Z Trial code - extending from m230900c1.c
  *
  *                *** end of m230900c2.c ***                      */
